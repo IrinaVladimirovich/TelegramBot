dc873016dbe80be426c93ca409820897bb5a6c2e
Irina <125303048+IrinaVladimirovich@users.noreply.github.com>
2023-06-03 14:22:46 +0300

    Create cjnfig.py

---
+++ cjnfig.py
@@ -0,0 +1,10 @@
+TOKEN = "6139128092:AAFy18hrDt1RyQgRxR-nM2AUrxeJdei61JA"
+keys = {
+    "биткоин": "BTC",
+    "эфириум": "ETH",
+    "доллар": "USDC",
+    "тезер": "USDT",
+    "рипл": "XRP",
+    "байненс": "BNB",
+    "евро": "EUR"
+}
